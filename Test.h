0949c09feed2c6fc6566ef3197c494df13cefef8
luketerry15 <luketerry@utexas.edu>
2015-05-11 15:27:24 -0500

    Modifying Test.h

--- Test.h
+++ Test.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 void printNum(int val){
 	printf("%d\n", val);
 }
@@ -8,19 +9,24 @@ void printWord(char* src){
 	printf("%s\n", src);
 }
 
-void printBothWords(char* src1, char* src2){
-	printf("%s, %s\n", src1, src2);
-}
-
-/*Takes 2 input values, 2 words and one terminate value
-  For every number from 1 to the terminate value:
+/*Takes 2 input values, and 2 words
+  For every number from 100:
   1) if k is divisible by input value 1 then print word 1
   2) if k is divisible by input value 2 then print word 2
   3) if k is divisible by both value 1 and 2, then print both words */
-void run(int val1, char* word1, int val2, char* word2, int terminate){
-	for (int k = 1; k <= terminate; k += 1){
+void run(int val1, char* word1, int val2, char* word2){
+	if (val1 > 100 || val2 > 100){ // testing both values are less than 100
+		printf("Sorry, please use a value less than 100\n");
+		return;
+	}
+	if (word1 == nullptr || word2 == nullptr){ // testing both words aren't null pointers
+		printf("Sorry, you can't use a null string\n");
+		return;
+	}
+	
+	for (int k = 1; k <= 100; k += 1){
 		if (k% val1 == 0 && k % val2 == 0){
-			printBothWords(word1, word2);
+			printf("%s, %s\n", word1, word2);
 		}
 		else if (k % val1 == 0){
 			printWord(word1);
